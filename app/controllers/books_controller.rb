40834dc911c0ee2c50227e0f4b0da996933bd6da
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-09-25 13:09:42 +0000

    [Break] 3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,21 @@ class BooksController < ApplicationController
   def new
 
   end
+  
+  # インスタンス変数を使う時
+  # ①新規投稿用に空のモデルを用意したい
+  # ②投稿一覧を表示したい
+  # ③詳細画面で対応するデータを表示したい
+  # ローカル変数はデータのみを保存する処理をしたい時に使う
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id) #投稿が成功するとそのページへ行く
+    @book = Book.new(book_params)
+    if @book.save
+      # flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id) #投稿が成功するとそのページへ行く
     else
-      render :new
-      flash[:notice] = "投稿に失敗しました。"
+      @books = Book.all
+      render :index # indexの画面に行く
     end
   end
 
