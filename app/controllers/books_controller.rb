961bd54a8caac88f88050f700cab29c2e591cc89
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-09-26 13:02:15 +0000

    [Break] 3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      # flash[:notice] = "Book was successfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id) #投稿が成功するとそのページへ行く
     else
       @books = Book.all
@@ -34,9 +34,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id) #投稿が成功するとそのページへ行く
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
